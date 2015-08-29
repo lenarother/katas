2be32b9f34343b858f04059ac0572c28f5a35ff4
lenarother <rother.magdalena@gmail.com>
2015-08-29 21:07:45 +0200

    Add game description to the page.

--- fizz_buzz_app/functional_test.py
+++ fizz_buzz_app/functional_test.py
@@ -5,18 +5,21 @@ class FizzBuzzAcceptanceTests(TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		self.browser.get('http://localhost:8000')
 
 	def tearDown(self):
 		self.browser.quit()
-	
+
 	def test_page_title(self):
 		# Wojtek wants to play fizz buzz.
 		# He goes to https:127.0.0:8000/ and sees a page titled FizzBuzz.
-		self.browser.get('http://localhost:8000')
 		self.assertIn("FizzBuzz", self.browser.title)
 
+	def test_page_content(self):
+		# The page gives short explanation "FizzBazz is ...".
+		body = self.browser.find_element_by_tag_name('body')
+		self.assertIn('FizzBuzz is', body.text)
 
-# The page gives short explanation "FizzBazz is ...".
 # There is a also an input window and send batton.
 # After sending a 3, ther is a Fizz displayed on the page.
 # After sending a 5, ther is a Buzz diplayed.
