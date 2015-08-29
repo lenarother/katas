6618c3e6d3d3528b434782336f9d574b58c2bbde
lenarother <rother.magdalena@gmail.com>
2015-08-29 10:21:17 +0200

    Refactor tests to easily test different chop implementations.

--- binary_search_test.py
+++ binary_search_test.py
@@ -1,30 +1,38 @@
 from unittest import TestCase, main
-from binary_search import recoursive_chop
+from binary_search import (recoursive_chop,
+                           while_chop,)
 
 class BinarySortTests(TestCase):
 
+    def assertChop(self, chop):
+        self.assertEqual(-1, chop(3, []))
+        self.assertEqual(-1, chop(3, [1]))
+        self.assertEqual(0,  chop(1, [1]))
+        #
+        self.assertEqual(0,  chop(1, [1, 3, 5]))
+        self.assertEqual(1,  chop(3, [1, 3, 5]))
+        self.assertEqual(2,  chop(5, [1, 3, 5]))
+        self.assertEqual(-1, chop(0, [1, 3, 5]))
+        self.assertEqual(-1, chop(2, [1, 3, 5]))
+        self.assertEqual(-1, chop(4, [1, 3, 5]))
+        self.assertEqual(-1, chop(6, [1, 3, 5]))
+        #
+        self.assertEqual(0,  chop(1, [1, 3, 5, 7]))
+        self.assertEqual(1,  chop(3, [1, 3, 5, 7]))
+        self.assertEqual(2,  chop(5, [1, 3, 5, 7]))
+        self.assertEqual(3,  chop(7, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(0, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(2, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(4, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(6, [1, 3, 5, 7]))
+        self.assertEqual(-1, chop(8, [1, 3, 5, 7]))
+
     def test_recoursive_chop(self):
-        self.assertEqual(-1, recoursive_chop(3, []))
-        self.assertEqual(-1, recoursive_chop(3, [1]))
-        self.assertEqual(0,  recoursive_chop(1, [1]))
-        #
-        self.assertEqual(0,  recoursive_chop(1, [1, 3, 5]))
-        self.assertEqual(1,  recoursive_chop(3, [1, 3, 5]))
-        self.assertEqual(2,  recoursive_chop(5, [1, 3, 5]))
-        self.assertEqual(-1, recoursive_chop(0, [1, 3, 5]))
-        self.assertEqual(-1, recoursive_chop(2, [1, 3, 5]))
-        self.assertEqual(-1, recoursive_chop(4, [1, 3, 5]))
-        self.assertEqual(-1, recoursive_chop(6, [1, 3, 5]))
-        #
-        self.assertEqual(0,  recoursive_chop(1, [1, 3, 5, 7]))
-        self.assertEqual(1,  recoursive_chop(3, [1, 3, 5, 7]))
-        self.assertEqual(2,  recoursive_chop(5, [1, 3, 5, 7]))
-        self.assertEqual(3,  recoursive_chop(7, [1, 3, 5, 7]))
-        self.assertEqual(-1, recoursive_chop(0, [1, 3, 5, 7]))
-        self.assertEqual(-1, recoursive_chop(2, [1, 3, 5, 7]))
-        self.assertEqual(-1, recoursive_chop(4, [1, 3, 5, 7]))
-        self.assertEqual(-1, recoursive_chop(6, [1, 3, 5, 7]))
-        self.assertEqual(-1, recoursive_chop(8, [1, 3, 5, 7]))
+        self.assertChop(recoursive_chop)
+
+    def test_while_chop(self):
+        self.assertChop(while_chop)
+
 
 if __name__ == '__main__':
     main()
