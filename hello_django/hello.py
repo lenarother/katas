26bcaecc85c48b43c89939d0b0b0cf93c48af21f
lenarother <rother.magdalena@gmail.com>
2015-09-29 00:52:31 +0200

    Use gunicorn

--- hello_django/hello.py
+++ hello_django/hello.py
@@ -15,6 +15,7 @@ settings.configure(
 
 
 from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 def index(request):
@@ -25,6 +26,7 @@ urlpatterns = (
     url(r'^$', index),
 )
 
+application = get_wsgi_application()
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
