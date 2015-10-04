0fe2df870ba71559bacf55fecf38d34bb0051514
Magdalena Rother <rother.magdalena@gmail.com>
2015-10-04 14:54:01 +0200

    Add django project for creating placeholder images
    Start project using single-file-template. Add second view function for displaying images (no code yet).

---
+++ placeholder/placeholder.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+from django.conf import settings
+
+DEBUG = os.environ.get('DEBUG', 'on') == 'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', '_4v)d6cis6um^t0=nas@eato*r#td&xw9k1jv-%zwzy+cng_wl')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+def placeholder(request, width, height):
+    return HttpResponse('Ok')
+
+def index(request):
+    salutation = 'Hello {}'.format(__file__.replace('.py',''))
+    return HttpResponse(salutation)
+
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+application = get_wsgi_application()
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
+
+    # Run application development server:
+    # python hello.py runserver
+    # Run application production server:
+    # gunicorn hello --log-file=-
+    # To use it as a project template:
+    # django-admin-py startproject foo --template=project_name
