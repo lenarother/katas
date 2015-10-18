a6279107fe1e5697e8b1077af2e282387b6152fa
lenarother <rother.magdalena@gmail.com>
2015-10-18 23:07:09 +0200

    Add urls to static page generator app

--- static_site_generator/sitebuilder/urls.py
+++ static_site_generator/sitebuilder/urls.py
@@ -1,1 +1,10 @@
-urlpatterns = ()
+form django.conf.urls import url
+
+from .views import page
+
+
+urlpatterns = (
+    url(r'^(?<slug>[\w./-]+)/$', page, name='page'),
+    url(r'^$', page, name='homepage'),
+
+)
