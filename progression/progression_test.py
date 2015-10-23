2fc47e7b6c6e2a93b4bf1f47104bbeee856d981c
lenarother <rother.magdalena@gmail.com>
2015-10-23 11:30:15 +0200

    Add test for more advanced case - check whether list contains fragment of any progression

--- progression/progression_test.py
+++ progression/progression_test.py
@@ -3,8 +3,37 @@ from unittest import TestCase, main
 from progression import is_progression
 
 
+class HasProgressionTests(TestCase):
 
-class ProgressionTests(TestCase):
+    def test_three_numbers(self):
+        self.assertTrue(has_progresion([1, 2, 3]))
+        self.assertTrue(has_progression([1, 5, 9]))
+        self.assertFalse(has_progression([1, 5, 10]))
+
+    def test_to_short_input(self):
+        self.asertFalse(has_progression([]))
+        self.assertFalse(has_progression([1]))
+        self.assertFalse(has_progression([1, 2]))
+        self.assertFalse(has_progression([1.0, 1.5]))
+
+    def test_unordered_list(self):
+        self.assertTrue(has_progression([3, 2, 1]))
+        self.assertFalse(has_progression([10, 9, 7]))
+
+    def test_long_list(self):
+        self.assertTrue(has_progression([1, 2, 3, 7]))
+        self.assertTrue(has_progression([7, 1, 2, 3]))
+        self.assertTrue(has_progression([7, 9, 121, 300, 800, 111, 3, 2, 1]))
+        self.assertTrue(has_progression([3, 7, 8, 1, 15, 5, 2]))
+        self.assertFalse(has_progression([1, 2, 8, 11, 56, 100, 121]))
+
+    def test_floats(self):
+        self.asertTrue(has_progression([1.0, 15.1, 2.1, 1.1, 3.1]))
+        self.assertTrue(has_progression([1.3, 4.0, 1.1, 2.3, 1.1, 1.2]))
+        self.assertFalse(has_progression([9.0, 1.1, 5.5, 20.7, 8.8]))
+
+
+class IsProgressionTests(TestCase):
 
     def test_three_numbers(self):
         self.assertTrue(is_progression([1, 2, 3]))
