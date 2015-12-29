f05e35e88783dff009956d3cebfde78d572ff0dc
lenarother <rother.magdalena@gmail.com>
2015-12-29 11:50:37 +0100

    Add Task and User API endpoints

--- rest_api/scrum/board/views.py
+++ rest_api/scrum/board/views.py
@@ -1,7 +1,10 @@
 from rest_framework import authentication, permissions, viewsets
 
 from .models import Sprint
-from .serializers import SprintSerializer
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+
+
+User = get_user_model() 
 
 
 class DefaultsMixin(object):
@@ -21,8 +24,24 @@ class DefaultsMixin(object):
 	paginate_by_param = 'page_size'
 	max_paginate_by = 100
 
+
 class SprintViewSet(DefaultMixin, viewsets.ModelVoiewSet):
 	"""API endpoint for listing and creating sprints."""
 
 	queryset = Sprint.objects.order_by('end')
 	serializer_class = SprintSerializer
+
+
+class TaskViewSet(DefaultMixin, viewsets.ModelVoiewSet):
+	"""API endpoint for listing and creating tasks."""
+
+	queryset = Task.objects.all()
+	serializer_class = TaskSerializer
+
+class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
+	"""API endpoint for listing users."""
+
+	lookup_field = User.USERNAME_FIELD
+	lookup_url_kwarg = User.USERNAME_FIELD
+	queryset = User.objects.order_by(User.USERNAME_FIELD)
+	serializer_class = UserSerializer
