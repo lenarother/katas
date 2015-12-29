39a4f897f1872857ba1795985029867ce16b8ef3
lenarother <rother.magdalena@gmail.com>
2015-12-29 12:51:02 +0100

    Add more links

--- rest_api/scrum/board/serializers.py
+++ rest_api/scrum/board/serializers.py
@@ -46,8 +46,17 @@ class TaskSerilizer(serializers.ModelSerializer)
         return {
             'self': reverse('task-detail',
                 kwargs={'pk': obj.pk}, request=request),
+            'sprint': None,
+            'assigned':None
         }
-
+        if obj.sprint_id:
+            links['sprint'] = reverse('sprint-detail',
+                kwargs={'pk': obj.sprint_id}, request=request)
+        if obj.assigned:
+            links['assigned'] = reverse('user-detail',
+                kwargs={User.USERNAME_FIELD: obj.assigned}, request=request)
+        return links
+        
 
 class UserSerializer(serializers.ModelSerializer):
 
