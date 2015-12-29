6eb664dd4f615e3f22c3b988335dada3bb0ad0cc
lenarother <rother.magdalena@gmail.com>
2015-12-29 14:03:39 +0100

    Add backend filters

--- rest_api/scrum/board/views.py
+++ rest_api/scrum/board/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import authentication, permissions, viewsets
+from rest_framework import authentication, permissions, viewsets, filters
 
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
@@ -10,40 +10,48 @@ User = get_user_model()
 
 
 class DefaultMixin(object):
-	"""
-	Default settings for view authentication, permissions,
+    """
+    Default settings for view authentication, permissions,
     filtering and paginataion.
-	"""
+    """
 
-	authentication_classes = (
-		authentication.BasicAuthentication,
-		authentication.TokenAuthentication,
-	)
-	permission_classes= (
-		permissions.IsAuthenticated,
-	)
-	paginate_by = 25
-	paginate_by_param = 'page_size'
-	max_paginate_by = 100
-
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+    )
+    permission_classes= (
+        permissions.IsAuthenticated,
+    )
+    paginate_by = 25
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
+    filter_backends = (
+        filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
 
 class SprintViewSet(DefaultMixin, viewsets.ModelViewSet):
-	"""API endpoint for listing and creating sprints."""
+    """API endpoint for listing and creating sprints."""
 
-	queryset = Sprint.objects.order_by('end')
-	serializer_class = SprintSerializer
-
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+    search_fields = ('name', )
+    ordering_fields = ('end', 'name', )
 
 class TaskViewSet(DefaultMixin, viewsets.ModelViewSet):
-	"""API endpoint for listing and creating tasks."""
+    """API endpoint for listing and creating tasks."""
 
-	queryset = Task.objects.all()
-	serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    search_fields = ('name', 'description', )
+    ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
 
 class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
-	"""API endpoint for listing users."""
+    """API endpoint for listing users."""
 
-	lookup_field = User.USERNAME_FIELD
-	lookup_url_kwarg = User.USERNAME_FIELD
-	queryset = User.objects.order_by(User.USERNAME_FIELD)
-	serializer_class = UserSerializer
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
+    search_fields = (User.USERNAME_FIELD, )
