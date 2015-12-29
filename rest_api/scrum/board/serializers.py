db81ea969d2fd646de900889e162c9acf130a840
lenarother <rother.magdalena@gmail.com>
2015-12-29 12:56:30 +0100

    Fix bug in serializer

--- rest_api/scrum/board/serializers.py
+++ rest_api/scrum/board/serializers.py
@@ -43,7 +43,7 @@ class TaskSerilizer(serializers.ModelSerializer)
 
     def get_links(self, obj):
         request = self.context['request']
-        return {
+        links = {
             'self': reverse('task-detail',
                 kwargs={'pk': obj.pk}, request=request),
             'sprint': None,
@@ -56,7 +56,7 @@ class TaskSerilizer(serializers.ModelSerializer)
             links['assigned'] = reverse('user-detail',
                 kwargs={User.USERNAME_FIELD: obj.assigned}, request=request)
         return links
-        
+
 
 class UserSerializer(serializers.ModelSerializer):
 
